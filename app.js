4b4df4043b4e1fe2db873ef245f0ee0dbad3529c
Hamza405 <hamzash863@gmail.com>
2022-09-08 16:40:06 -0400

    connect mongodb

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+console.log("dddd");
 app.get("/", (req, res) => res.send("Test"));
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => console.log("Connected DataBase"))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
