dc76c08455cd7b7f2d9fece458f575037d526bbc
Hamza405 <hamzash863@gmail.com>
2022-09-08 16:55:33 -0400

    Implement project models

---
+++ models/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    photo: { type: String, required: false },
+    userName: { type: String, required: true },
+    categories: { type: Array, required: false },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
