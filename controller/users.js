5b189e48653040560276c63f9966a546e4793f2d
Hamza405 <hamzash863@gmail.com>
2022-09-09 04:39:02 -0400

    Implement delete account api

--- controller/users.js
+++ controller/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 exports.updateUser = async (req, res) => {
@@ -22,5 +23,26 @@ exports.updateUser = async (req, res) => {
     }
   } else {
     res.status(401).json({ error: "You can update only your account!" });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  if (req.body.userId === req.params.userId) {
+    try {
+      const user = User.findById(req.params.userId);
+      try {
+        if (user) {
+          await Post.deleteMany({ userName: user.userName });
+        }
+        await User.findByIdAndRemove(req.params.userId);
+        res.status(200).json("User has been deleted");
+      } catch (err) {
+        res.status(500).json({ error: err });
+      }
+    } catch (err) {
+      res.status(404).json({ error: "User not found!" });
+    }
+  } else {
+    res.status(401).json({ error: "You can delete only your account!" });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const controller = require("../controller/user");
+const controller = require("../controller/users");
 
 // UPDATE USER
 router.post("/:userId", controller.updateUser);
 
 // DELETE USER
+router.delete("/:userId", controller.deleteUser);
 
 module.exports = router;
