3ffd493322b20d931325e0292c73a9b382b4782b
Hamza405 <hamzash863@gmail.com>
2022-09-09 17:55:38 -0400

    Implement upload images api

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-
-mongoose
-  .connect("mongodb://localhost:27017/hblogy")
-  .then(() => console.log("Connected DataBase"))
-  .catch((err) => console.log(err));
-app.use(express.json());
+const multer = require("multer");
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
 const postRouter = require("./routes/posts");
 const categoriesRouter = require("./routes/categories");
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/hblogy")
+  .then(() => console.log("Connected DataBase"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, "hello.jpeg");
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api", authRouter);
 app.use("/api/users", userRouter);
